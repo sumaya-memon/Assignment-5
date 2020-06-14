fd75de26776fa259459930c7d6bea247a13e8787
sumaya-memon <64688015+sumaya-memon@users.noreply.github.com>
2020-06-14 05:52:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,133 @@
+/chapter 1 Q:1
+
+//alert("welcome to my web")
+
+//chapter 1 Q:2
+
+//alert("Error!Please enter a valid password.")
+
+//chapter 1 Q:3
+
+//alert("welcome to js land\n Happy coding!")
+
+//chaterp 1 Q:4
+
+//alert("Welcome to JS Land..")
+//alert("Happy Coding!")
+
+//chaterp 1 Q:5
+
+// alert("Hello... I can run JS through my console")
+
+//chapter 1 Q:6
+
+// {/* <script>
+// document.getElementById("demo").innerHTML = 5 + 6;
+// </script> */}
+
+////chapter 1 Q:7(in HTML)
+
+
+
+//ch:2 Q1
+//var Name= "YUSRA";
+alert(Name);
+
+//CHAPTER 2 Q:2
+// var name = "myname";
+// var x = "sumaya" + " " + "memon";
+
+//CHAPTER 2 Q:3
+
+// var message = "Hellow world"
+// alert(message);
+
+
+//CHAPTER 2 Q:4
+
+//var x = "John Doe";
+//var y= "15 years old";
+//var z="certified Mobile application Development";
+//alert(x);
+//alert(y);
+//alert(z);
+
+//CHAPTER 2 Q:5
+
+//alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+//CHAPTER 2 Q:6
+
+// var email = "sumaya" + "@" + "gmail.com"
+// alert(email)
+
+//CHAPTER 2 Q:7
+
+// var book = "A smarter way to learn JavaScript"
+// alert(book)
+
+//CHAPTER 2 Q:8
+
+// document.write("YAH i can writ document through java script")
+
+
+//CHAPTER 2 Q:9
+// var a ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a)
+
+
+
+// =chapter3 task 1
+
+// var age = "I am 16 years old"
+// alert(age)
+
+// =chapter3 task 2
+
+// var a ="You have visited this site 10 times"
+// alert(a)
+
+// =chapter3 task 3
+
+// var birthyear = "my birth year is 2001 \n date type variable is number"
+// document.write(birthyear)
+
+// =chapter3 task 4
+// var Visitorsname ="yusra"
+// var Product  = "T-shirts"
+// var  Quantity ="5"
+// document.write( Visitorsname +  "   " +  "ordered" + Quantity + "  "  + Product + "on XYZ Clothing store") 
+
+
+// =chapter4 task 1
+
+// var a,b,c
+// var a = yusra , b = essa , c = daudpota
+
+
+// =chapter4 task 2
+
+//  var a,b,c,d,e
+//  var a = "yusra" , b = "1", c = "$", d = "_h" , e = "essa"
+
+//chapter 4 task 3
+//  var f,g,h,i,j
+//  var f =hgy , g = 1 , h = 0, i = _2 , j = 4
+//  <h1> Rules for naming JS variables</h1>
+
+
+//  document.write("Variable" + "names " +  "can " + "only "   +   "contain"    +  "" 
+//  +a+ ""+ b +""  + c +""+d +""+e)
+//  document.write("Variable" + "names " +  "can " + "only "   +   "contain"    +  "" 
+//  +f+ ""+ g +""  + h +""+d  + "" + i + "" +""+j)
+//  document.write("Variable" + "names " +  "must  " + "begain  "   +   "with"    +  "CApital letter")
+//  document.write("Variable" + "names " +  "are " + "case  "   +   "sensitive"    )
+//  document.write("Variable" + "names " +  "shouldnot " + "be  " + "a  " + "JS   " + "Variable  "   )
+
+
+
+// =chapter5 task 1
+// var a = 3
+// var b = 5
+// var sum = a + b
+// document.write("sum" +  "   " +  "of" + "   " + a + "  "  + "and" +"  " + b +"  "+  "is"+ sum) 
